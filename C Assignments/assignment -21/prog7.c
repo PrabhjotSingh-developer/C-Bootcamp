389303c9369bf1e64f8dbac2efcc72fab6eed24a
PrabhjotSingh-developer <95993712+PrabhjotSingh-developer@users.noreply.github.com>
2022-08-25 19:30:33 +0530

    Update prog7.c

--- C Assignments/assignment -21/prog7.c
+++ C Assignments/assignment -21/prog7.c
@@ -1,25 +1,32 @@
 // 7. Write a program to calculate the difference between two time periods.
-#include<stdio.h>
+#include <stdio.h>
 struct time
 {
-    int hh,mm,sec;
+   int hh, mm, sec;
 };
 int main()
 {
-  struct time t1,t2,t3;
-  printf("Enter the time Period first\n");
-   scanf("%d%d%d",&t1.hh,&t1.mm,&t1.sec);
-    printf("Enter the time Period Second\n");
-   scanf("%d%d%d",&t2.hh,&t2.mm,&t2.sec);
-   int f1,f2,f3;
-   f1 = (t1.hh*60)+t1.mm+(t1.sec/60);
-   f2 = (t2.hh*60)+t2.mm+(t2.sec/60);
-   f3=f2-f1;
-    printf("%d ",f3);
-   if(f3<0)
-      f3*-1;
-   printf("Time period is \n");
-   t3.hh = f3/60;
-   t3.mm=f3 % 60;
-   printf("%d : %d\n",t3.hh,t3.mm);
+   struct time t1, t2, t3;
+   printf("Enter the starting time Period \n");
+   scanf("%d%d%d", &t1.hh, &t1.mm, &t1.sec);
+   printf("Enter the end time Period\n");
+   scanf("%d%d%d", &t2.hh, &t2.mm, &t2.sec);
+   printf("%d : %d : %d \n", t1.hh, t1.mm, t1.sec);
+   printf("%d : %d : %d \n", t2.hh, t2.mm, t2.sec);
+   if (t2.sec < t1.sec)
+   {
+      t2.mm = t2.mm - 1;
+      t2.sec += 60;
+   }
+   if (t2.mm < t1.mm)
+   {
+      t2.hh = t2.hh - 1;
+      t2.mm = t2.mm + 60;
+   }
+
+   t3.hh = t2.hh - t1.hh;
+   t3.mm = t2.mm - t1.mm;
+   t3.sec = t2.sec - t1.sec;
+   printf("\n The difference between two time period is \n");
+       printf("%d : %d : %d \n", t3.hh, t3.mm, t3.sec);
 }
